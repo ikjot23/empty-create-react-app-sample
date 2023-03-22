be6d75e3347f7c001d732f6994a68741c6f89b49
ikjot23 <128335856+ikjot23@users.noreply.github.com>
2023-03-22 08:48:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>DriverPass</h2>
-        </div>
         <p className="App-intro">
           Coming Soon
         </p>
